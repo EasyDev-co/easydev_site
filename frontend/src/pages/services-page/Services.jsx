a67f694b43de262ec286e461fa3a394232a2ae90
loach99 <constantinent@mail.ru>
2024-08-13 17:10:54 +0300

    PRODUCT-55: сверстал страницу услуг с адаптивом

--- frontend/src/pages/services-page/Services.jsx
+++ frontend/src/pages/services-page/Services.jsx
@@ -1,15 +1,28 @@
 import { Link } from 'react-router-dom'
 import { services } from '../../mockData/services'
+import { useWindowWidth } from '@react-hook/window-size'
+import styles from './styles/Services.module.scss'
 
 export const ServicesPage = () => {
+  const width = useWindowWidth()
   return (
     <div>
-      <h1>Services page</h1>
-      <ul>
+
+      <div className={styles.breadcrumbs}>
+        {width < 480 ? 'HOME' : 'ГЛАВНАЯ'} / <span>ЭКСПЕРТНОСТЬ</span>
+      </div>
+      <h1 className={styles.service__title}>Экспертность</h1>
+      <ul className={styles.services}>
         {services.map((elem) => (
-          <li key={elem.id}>
-            <Link to={`/services/${elem.id}`}>{elem.service}</Link>
-          </li>
+          <div key={elem.id} className={styles.service__container}>
+            <div className={styles.service__shield}></div>
+            <li>
+              <Link to={`/services/${elem.id}`}>{elem.service}</Link>
+            </li>
+            <div className={styles.picture_container}>
+                  
+            </div>
+          </div>
         ))}
       </ul>
     </div>
