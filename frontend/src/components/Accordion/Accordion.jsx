a6863198c2d3f096c96ca7638bd082805089bc86
loach99 <constantinent@mail.ru>
2024-08-13 20:05:12 +0300

    PRODUCT-58: сверстал компонент аккордеон

---
+++ frontend/src/components/Accordion/Accordion.jsx
@@ -0,0 +1,25 @@
+import styles from './styles/Accordion.module.scss'
+import { accordion } from '../../mockData/accordion';
+
+const Accordion = () => {
+    return (
+        <div className={styles.accordion__container}>
+            {accordion.map(elem => {
+                return (
+                    <div key={elem.id} className={styles.accordion__tabs}>
+                        <input id={elem.id} type="checkbox" name="tabs" />
+                        <label htmlFor={elem.id}>
+                            <div className={styles.number}>{elem.id <= 9 ? `0${elem.id}`: elem.id}</div>
+                            {elem.title}
+                        </label>
+                        <div className={styles.accordion__tab_content}>
+                            <p>{elem.text}</p>
+                        </div>
+                    </div>
+                )
+            })}
+        </div>
+    );
+}
+
+export default Accordion;
--- frontend/src/pages/service-page/Service.jsx
+++ frontend/src/pages/service-page/Service.jsx
@@ -1,6 +1,8 @@
 import { useWindowWidth } from '@react-hook/window-size'
 import { useLocation, useParams } from 'react-router-dom'
 import styles from './styles/Service.module.scss'
+import BlueButton from '../../components/buttons/BlueButton/BlueButton'
+import Accordion from '../../components/Accordion/Accordion'
 
 export const ServicePage = () => {
   const { serviceId } = useParams()
@@ -9,9 +11,36 @@ export const ServicePage = () => {
 
   const width = useWindowWidth()
   return <div>
+
     <div className={styles.breadcrumbs}>
-      {width < 480 ? 'HOME' : 'ГЛАВНАЯ'} / <span>ЭКСПЕРТНОСТЬ</span> / <span>{services}</span>
+      {width < 480 ? 'HOME' : 'ГЛАВНАЯ'} / ЭКСПЕРТНОСТЬ / <span>{services}</span>
     </div>
-    <h1 className={styles.cases__title}>Кейсы (19/24)</h1>
+    <h1 className={styles.service__title}>{services}</h1>
+    <div className={styles.service_title_wrap}>
+      <span className={styles.service__title__desc}>
+        Принимайте правильные управленческие решения
+        на основе данных аудита. Описание и дополнительная информация.
+      </span>
+      <BlueButton text={'Заказать услугу'} />
+    </div>
+    <div className={styles.about__service_wrap}>
+      <div className={styles.about__service}>
+        <div>
+          <div>200 +</div>
+          <div>Выполненных проектов</div>
+        </div>
+        <div>
+          <div>85 +</div>
+          <div>Разработанных стратегий тестирования</div>
+        </div>
+        <div>
+          <div>30 +</div>
+          <div>Управляемых проектов</div>
+        </div>
+      </div>
+    </div>
+    <div className={styles.skills_container}>
+        <Accordion/>
+    </div>
   </div>
 }
