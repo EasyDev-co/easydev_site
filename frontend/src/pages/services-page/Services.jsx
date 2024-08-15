9200b2992a256d01a3b1885a632771362a2d4b5b
loach99 <constantinent@mail.ru>
2024-08-15 11:46:51 +0300

    PRODUCT-55: поправил стили согласно рекомендациям на CR

--- frontend/src/pages/services-page/Services.jsx
+++ frontend/src/pages/services-page/Services.jsx
@@ -6,25 +6,26 @@ import styles from './styles/Services.module.scss'
 export const ServicesPage = () => {
   const width = useWindowWidth()
   return (
-    <div>
-
-      <div className={styles.breadcrumbs}>
-        {width < 480 ? 'HOME' : 'ГЛАВНАЯ'} / <span>ЭКСПЕРТНОСТЬ</span>
-      </div>
-      <h1 className={styles.service__title}>Экспертность</h1>
-      <ul className={styles.services}>
-        {services.map((elem) => (
-          <div key={elem.id} className={styles.service__container}>
-            <div className={styles.service__shield}></div>
-            <li>
-              <Link to={`/services/${elem.id}`}>{elem.service}</Link>
-            </li>
-            <div className={styles.picture_container}>
-                  
+    <main className={styles.main}>
+      <section className={styles.service}>
+        <div className={styles.breadcrumbs}>
+          {width < 480 ? 'HOME' : 'ГЛАВНАЯ'} / <span>ЭКСПЕРТНОСТЬ</span>
+        </div>
+        <h1 className={styles.service__title}>Экспертность</h1>
+        <ul className={styles.service__wrapp}>
+          {services.map((elem) => (
+            <div key={elem.id} className={styles.service__container}>
+              <div className={styles.service__shield}></div>
+              <li>
+                <Link to={`/services/${elem.id}`}>{elem.service}</Link>
+              </li>
+              <div className={styles.picture_container}>
+              </div>
             </div>
-          </div>
-        ))}
-      </ul>
-    </div>
+          ))}
+        </ul>
+      </section>
+
+    </main>
   )
 }
