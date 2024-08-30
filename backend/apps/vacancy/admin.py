83cc15b6a1d80e19e0a46c5af38a5b20eac5b174
Kurkov Nikita <143032451+Wahtarezx@users.noreply.github.com>
2024-08-30 18:09:33 +0300

    Добавил поле is_active в админ панель вакансий

--- backend/apps/vacancy/admin.py
+++ backend/apps/vacancy/admin.py
@@ -6,12 +6,14 @@ from apps.vacancy.models import Vacancy, VacancyForm
 
 @admin.register(Vacancy)
 class VacancyAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'title', 'post', 'description_short', 'salary_fork_lt', 'salary_fork_gt', 'image')
+    list_display = (
+        'pk', 'title', 'post', 'description_short', 'salary_fork_lt', 'salary_fork_gt', 'image', 'is_active',
+    )
     search_fields = ('pk', 'title', 'post')
     fieldsets = [
         (None, {
             'fields': (
-                'title', 'post', 'salary_fork_lt', 'salary_fork_gt', 'image', 'preview',
+                'title', 'post', 'salary_fork_lt', 'salary_fork_gt', 'image', 'preview', 'is_active',
             ),
         }),
     ]
