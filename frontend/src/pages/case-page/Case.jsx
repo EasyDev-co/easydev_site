4412099ac9202fc87ad81f8c62cbc153b0965041
Alex-Viet <alex.night.power@gmail.com>
2024-09-03 18:17:28 +0300

    PRODUCT-59: Правка верстки страницы отдельного кейса

--- frontend/src/pages/case-page/Case.jsx
+++ frontend/src/pages/case-page/Case.jsx
@@ -1,5 +1,4 @@
 import { useLocation } from 'react-router-dom'
-import { useWindowWidth } from '@react-hook/window-size'
 import BlueButton from '../../components/buttons/BlueButton/BlueButton'
 import AccordionCases from '../../components/Accordions/AccordionCases'
 import { useRef } from 'react'
@@ -10,7 +9,6 @@ import { containerVariants } from '../../animations/variants'
 
 export const CasePage = () => {
   const location = useLocation()
-  const width = useWindowWidth()
   const { name, images } = location.state || {}
 
   const containersRef = useRef([])
@@ -21,28 +19,30 @@ export const CasePage = () => {
     <main className={styles.main}>
       <section className={styles.case}>
         <motion.div
+          className={styles.case__titleContainer}
           variants={containerVariants}
           custom={2}
           initial="initial"
           animate="animate"
         >
-          <h1 className={styles.case__tittle}>{name}</h1>
-          {width > 768 && <BlueButton text={'Посетить наш сайт'} />}
+          <h1 className={styles.case__title}>{name}</h1>
+          <BlueButton text={'Посетить сайт'} />
         </motion.div>
         <motion.div
-          className={styles.case__item}
+          className={styles.case__box}
           variants={containerVariants}
           custom={3}
           initial="initial"
           animate="animate"
         >
-          <div className={styles.case__item__accordion}>
+          <div className={styles.case__accordion}>
             <AccordionCases isCase data={location.state} />
           </div>
-          <motion.div className={styles.case__item__image}>
-            {images.slice(1).map((image, i) => {
+          <motion.div className={styles.case__images}>
+            {images.map((image, i) => {
               return (
                 <motion.div
+                  className={styles.case__image}
                   key={i}
                   ref={(el) => (containersRef.current[i] = el)}
                 >
