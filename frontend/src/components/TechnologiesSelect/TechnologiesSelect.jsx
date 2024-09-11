15e62ea34815af657ef7773a05874c8948aa4e95
Alex-Viet <alex.night.power@gmail.com>
2024-09-11 14:11:46 +0300

    PRODUCT-62: Правка верстки/стилей

---
+++ frontend/src/components/TechnologiesSelect/TechnologiesSelect.jsx
@@ -0,0 +1,54 @@
+import { useState } from 'react'
+import { technologies } from '../../mockData/technologies'
+import styles from './styles/TechnologiesSelect.module.scss'
+
+const TechnologiesSelect = () => {
+  const [headerData, setHeaderData] = useState({})
+  const [isActiveId, setIsActiveId] = useState(false)
+
+  const handleMouseHover = (data) => {
+    setHeaderData({
+      name: data.name,
+      desc: data.description,
+    })
+    setIsActiveId(data.id)
+  }
+
+  return (
+    <div className={styles.technologies}>
+      <div className={styles.technologies__infoContainer}>
+        <div className={styles.technologies__heading}>Технологии</div>
+        <div className={styles.technologies__info}>
+          <div>{headerData.name}</div>
+          <div>{headerData.desc}</div>
+        </div>
+      </div>
+      <div className={styles.technologies__container}>
+        {technologies.map((elem) => {
+          return (
+            <div
+              className={
+                elem.id === isActiveId
+                  ? styles.technologies__item_active
+                  : styles.technologies__item
+              }
+              role="button"
+              tabIndex="0"
+              key={elem.id}
+              onMouseEnter={() => handleMouseHover(elem)}
+              onMouseLeave={() => handleMouseHover({})}
+            >
+              <img
+                className={styles.technologies__img}
+                src={elem.image}
+                alt=""
+              />
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default TechnologiesSelect
--- frontend/src/pages/service-page/Service.jsx
+++ frontend/src/pages/service-page/Service.jsx
@@ -1,7 +1,7 @@
 import { useLocation } from 'react-router-dom'
 import BlueButton from '../../components/buttons/BlueButton/BlueButton'
 import Accordion from '../../components/Accordions/Accordion'
-import TecnologiesSelect from '../../components/TechnologiesSelect/TecnologiesSelect'
+import TechnologiesSelect from '../../components/TechnologiesSelect/TechnologiesSelect'
 import { accordion } from '../../mockData/accordion'
 import styles from './styles/Service.module.scss'
 
@@ -13,12 +13,12 @@ export const ServicePage = () => {
   return (
     <main className={styles.main}>
       <section className={styles.service}>
-        <h1 className={styles.service__title}>{name}</h1>
-        <div className={styles.service__title__wrap}>
-          <span className={styles.service__title__desc}>{description}</span>
+        <div className={styles.service__infoContainer}>
+          <h1 className={styles.service__title}>{name}</h1>
+          <p className={styles.service__desc}>{description}</p>
           <BlueButton text={'Заказать услугу'} />
         </div>
-        <div className={styles.service__about_wrap}>
+        <div className={styles.service__aboutContainer}>
           <div className={styles.service__about}>
             <div>
               <div>200 +</div>
@@ -35,13 +35,17 @@ export const ServicePage = () => {
           </div>
         </div>
         <div className={styles.service__skills}>
-          <h2>Скиллы</h2>
-          <Accordion data={accordion} />
+          <div className={styles.service__accordionContainer}>
+            <h2>Скиллы</h2>
+            <Accordion data={accordion} />
+          </div>
+          <TechnologiesSelect />
         </div>
-        <TecnologiesSelect />
-        <div className={styles.service__skills}>
-          <h2>Часто задаваемые вопросы</h2>
-          <Accordion data={accordion} />
+        <div className={styles.service__faq}>
+          <div className={styles.service__accordionContainer}>
+            <h2>Часто задаваемые вопросы</h2>
+            <Accordion data={accordion} />
+          </div>
         </div>
       </section>
     </main>
