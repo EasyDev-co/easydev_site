ab47f6583047993600b5bbabadf6878702f40227
timofejkazakov <t.kazakov@agora.ru>
2025-01-24 15:05:36 +0300

    Деплой

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -36,6 +36,20 @@ INSTALLED_APPS = [
     'apps.feedback.apps.FeedbackConfig',
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:3000",
+    "https://easy-dev.tech",
+    "http://localhost:3000",
+    "http://185.244.180.41:3000"
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://127.0.0.1:3000",
+    "http://185.244.180.41:3000",
+    "https://easy-dev.tech",
+    "http://localhost:3000",
+]
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
