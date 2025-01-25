72ccd60df981b8c48e4112226c3e8f278e798968
timofejkazakov <t.kazakov@agora.ru>
2025-01-25 15:34:57 +0300

    Контакты и хэдер

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom'
 import { Logo } from '../logo/Logo'
 import { ShowMenuButton } from '../buttons/show-menu-button/ShowMenuButton'
 import { useState } from 'react'
@@ -9,48 +8,82 @@ import BlueButton from '../buttons/BlueButton/BlueButton'
 import { NavLinks } from '../nav-links/NavLinks'
 import { motion } from 'framer-motion'
 import styles from './styles/Header.module.scss'
+import { AnimatePresence } from "framer-motion";
+import {Contacts} from "../modals/contacts/Contacts";
 
-export const Header = ({ setIsOpen }) => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false)
 
-  const width = useWindowWidth()
+export const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isContactsOpen, setIsContactsOpen] = useState(false);
+
+  const telegramUrl = 'https://t.me/easydev_egor';
+  const width = useWindowWidth();
 
   return (
     <>
       <motion.header
         className={styles.header}
         initial={{ opacity: 0, y: '-100%' }}
-        animate={{
-          opacity: 1,
-          y: 0,
-        }}
+        animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 1, ease: 'easeInOut', delay: 1 }}
       >
         <div className={styles.header__container}>
           <div className={styles.header__left}>
             <div className={styles.header__buttons}>
               <BlueButton
-                text={'Начать проект с easy'}
-                onClick={() => console.log('Начать проект!')}
+                text="Начать проект с easydev"
+                onClick={() => (window.location.href = telegramUrl)}
               />
-              <OriginButton text={'Контакты'} onClick={() => setIsOpen(true)} />
+              <OriginButton
+                text="Контакты"
+                onClick={() => setIsContactsOpen(true)}
+              />
             </div>
-            <div className={styles.header__phone}>+7 (913) 715 — 6372</div>
+            <div className={styles.header__phone}>+7 (917) 230 — 0481</div>
           </div>
+
           <div className={styles.header__logo}>
-            <Link to="/" onClick={() => setIsMenuOpen(false)}>
+            <a
+              href="/"
+              onClick={() => {
+                setIsMenuOpen(false);
+                setIsContactsOpen(false);
+              }}
+            >
               <Logo />
-            </Link>
+            </a>
           </div>
+
           <div className={styles.header__nav}>
             <NavLinks />
           </div>
+
           {width < 1366 && (
-            <ShowMenuButton isOpen={isMenuOpen} setIsOpen={setIsMenuOpen} />
+            <ShowMenuButton
+              isOpen={isMenuOpen}
+              setIsOpen={setIsMenuOpen}
+            />
           )}
         </div>
       </motion.header>
-      {isMenuOpen && <Menu isOpen={isMenuOpen} setIsOpen={setIsMenuOpen} />}
+
+      <AnimatePresence>
+        {isMenuOpen && (
+          <Menu
+            isOpen={isMenuOpen}
+            setIsOpen={setIsMenuOpen}
+          />
+        )}
+      </AnimatePresence>
+
+      <AnimatePresence>
+        {isContactsOpen && (
+          <Contacts
+            isOpen={isContactsOpen}
+            setIsOpen={setIsContactsOpen}
+          />
+        )}
+      </AnimatePresence>
     </>
-  )
-}
+  );
+};
